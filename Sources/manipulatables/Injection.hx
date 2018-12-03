78e0ebdfa0f75292293046329cca669d0932b0d5
JdH <JaiiDerHerr@hotmail.com>
2018-12-03 22:35:16 +0100

    use text constants

--- Sources/manipulatables/Injection.hx
+++ Sources/manipulatables/Injection.hx
@@ -4,6 +4,6 @@ import kha.Assets;
 
 class Injection extends UseableSprite {
 	public function new(px: Int, py: Int) {
-		super("Injection", Assets.images.spritze2, px, py);
+		super(Keys_text.INJECTION, Assets.images.spritze2, px, py);
 	}
 }
