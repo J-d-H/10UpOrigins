188f2c3233fb6f1f644f276f9f64bd9ed07fc66f
Christian Reuter <gamedev@aoe-maniac.de>
2018-12-03 20:50:57 +0100

    Cansnot picked up anymore.

--- Sources/manipulatables/Can.hx
+++ Sources/manipulatables/Can.hx
@@ -3,15 +3,19 @@ package manipulatables;
 import kha.Image;
 import kha.graphics2.Graphics;
 import kha2d.Scene;
+import kha2d.Sprite;
 
-class Can extends UseableSprite
+class Can extends Sprite
 {
 	private static inline var livetimeInFrames: Int = 120;
 	private var livetimeInFramesRemaining: Int = 0;
 
 	public function new(name: String, image: Image, px: Float, py: Float)
 	{
-		super(name, image, px - image.width / 2, py);
+		super(image);
+		x = px - image.width / 2;
+		y = py;
+		accy = 0;
 		livetimeInFramesRemaining = livetimeInFrames;
 		Scene.the.addOther(this);
 	}
